80799181e6a1a361ab941f68cbff74947caa5c44
votatthang <59831890+votatthang@users.noreply.github.com>
2024-01-18 23:09:43 +0100

    Update Research.js

--- src/Pages/Research/Research.js
+++ src/Pages/Research/Research.js
@@ -12,22 +12,30 @@ export default function Research() {
       Novel methods for mediation analysis based on recanting twins
     </div>
     <div className="contentText">
-      The presence of intermediate confounders, also called recanting witnesses, is a fundamen- tal challenge to the investigation of causal mechanisms in mediation analysis, preventing the identification of natural path-specific effects. In this project, we develop a novel method for mediation analysis in settings with intermediate confounding, with guarantees that the causal parameters measure the target individual-level mechanisms. The method is based on recently proposed ideas that view causality as the transfer of information, and thus replace recanting witnesses by draws from their conditional distribution. We present the assumptions required for identification of recanting twins effects under a standard structural causal model, and under recently proposed separable effects models. To estimate recanting-twin effects, we develop efficient semi-parametric strategies that allow the use of data driven methods in the estimation of the nuisance parameters. This is a joint work with Dr. Ivan Diaz and Dr. Kara Rudolph.
+      The presence of intermediate confounders, also called recanting witnesses, is a fundamental challenge to the investigation of causal mechanisms 
+      in mediation analysis, preventing the identification of natural path-specific effects. In this project, we develop a novel method for mediation 
+      analysis in settings with intermediate confounding, with guarantees that the causal parameters measure the target individual-level mechanisms. 
+        The method is based on recently proposed ideas that view causality as the transfer of information, and thus replace recanting witnesses by draws 
+        from their conditional distribution. This is a joint work with Dr. Ivan Diaz and Dr. Kara Rudolph.
     </div>
     <div className="title">
       Risk of Bias in Mediation Analysis (ROBIMA)
     </div>
     <div className="contentText">
-      During the last few years, the volume of mediation analyses conducted in heath care research has been tremendously expanded. Many critical appraisal checklists for mediation have thus been proposed, which greatly support applied researchers in evaluating the conduct and reporting of published mediation analyses in the literature. These checklists, however, are often not rigorously developed, nor adequately addressing important mediation-specific biases that potentially impact the validity of mediation findings. This raises important concerns in systematic reviews and meta-analyses of mediation analyses. ROBIMA - Risk of Bias in Mediation Analysis, is a new initiative that involves international experts in psychology, epidemiology and biostatistics, with aim to construct a consensus-based, high-quality tool for evaluating methodological biases in mediation analyses. This project involves Pr. Stijn Vansteelandt, Pr. Tyler Vanderweele, Pr. David MacKinnon, Dr. Hopin Lee, Dr. Aidan Cashin, among many others.
+      ROBIMA is a new initiative that involves international experts in psychology, epidemiology and biostatistics, with aim to construct a consensus-based, 
+      high-quality tool for evaluating methodological biases in mediation analyses. This project involves Pr. Stijn Vansteelandt, Pr. Tyler Vanderweele, Pr. 
+          David MacKinnon, Dr. Hopin Lee, Dr. Aidan Cashin, among many others.
     </div>
     <div className="title">
       Instrumented difference-in-differences
     </div>
     <div className="contentText">
-      In the standard difference-in-differences research design, the parallel trends assumption may be violated when the relationship between the exposure trend and the outcome trend is confounded by unmeasured confounders. Progress can be made if there is an exogenous variable that (i) does not directly influence the change in outcome means (i.e. the outcome trend) except through influencing the change in exposure means (i.e. the exposure trend), and (ii) is not related to the unmeasured exposure - outcome confounders on the trend scale. Such exogenous variable is called an instrument for difference-in-differences. In this project, we suggests novel multiplicative structural mean models for instrumented difference-in-differences, which allow one to identify and estimate the average treatment effect on continuous, count and rare binary outcomes, in the whole population or among the treated, when a valid instrument for difference-in-differences is available. This is a joint work with Pr. Dylan Small, Pr. Sean Hennessy, Pr. Stijn Vansteelandt, Dr. Ting Ye, Dr. Ashkan Ertefaie and Dr. Samrat Roy.
-    </div>
-    <div className="title">
-      Causal inference for rare events
-    </div>
+      In the standard difference-in-differences research design, the parallel trends assumption may be violated when the relationship between the exposure trend 
+      and the outcome trend is confounded by unmeasured confounders. Progress can be made if there is an exogenous variable that (i) does not directly influence 
+      the change in outcome means (i.e. the outcome trend) except through influencing the change in exposure means (i.e. the exposure trend), and (ii) is not 
+      related to the unmeasured exposure - outcome confounders on the trend scale. Such exogenous variable is called an instrument for difference-in-differences. 
+      In this project, we suggests novel multiplicative structural mean models for instrumented difference-in-differences. This is a joint work with Pr. Dylan Small, 
+      Pr. Sean Hennessy, Pr. Stijn Vansteelandt, Dr. Ting Ye, Dr. Ashkan Ertefaie and Dr. Samrat Roy.
+  </div>
   </div>;
 }
