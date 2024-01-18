c2f0595628737f0ca522cf0b92a9269710b88bdb
votatthang <59831890+votatthang@users.noreply.github.com>
2024-01-18 22:59:46 +0100

    Update AboutMe.js

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -8,8 +8,8 @@ export default function AboutMe() {
         <div className="shortDescription">
             I am a biostatistician with research interests in developing and applying causal inference methods to understand the benefice and risks of medical interventions on human’s health. 
             Previously, I received a BSc. (Hons) in Mathematics at The Open University (UK), a Master in Comparative Effectiveness Research at University Paris Cité (France) and a Pharm.D. at Hanoi University of Pharmacy (Vietnam).<br/> <br/> 
-            In 2020, I completed a PhD in Biostatistics jointly from Ghent University (Belgium) and University Paris Cité (France), under the supervision of Pr. <a href="https://www.ugent.be/dass/en/research/vansteelandt"> Stijn Vansteelandt</a> 
-            and Pr. <a href="https://cress-umr1153.fr/fr/teams/methods/member-raphael-porcher-254/"> Raphael Porcher</a>. My PhD research was funded by the European Union H2020 program, within the MiRoR (Methods in Research on Research) project. 
+            In 2020, I completed a PhD in Biostatistics jointly from Ghent University (Belgium) and University Paris Cité (France), under the supervision of Pr. <a href="https://www.ugent.be/dass/en/research/vansteelandt"> Stijn Vansteelandt</a> and 
+            Pr. <a href="https://cress-umr1153.fr/fr/teams/methods/member-raphael-porcher-254/"> Raphael Porcher</a>. My PhD research was funded by the European Union H2020 program, within the MiRoR (Methods in Research on Research) project. 
             After three years of postdoctoral training mentored by Pr. <a href="https://statistics.wharton.upenn.edu/profile/dsmall/#overview"> Dylan S. Small</a> and Pr. <a href="https://www.med.upenn.edu/apps/faculty/index.php/g275/p8396"> Sean Hennessy</a> at
             the Department of Statistics and Data Science, The Wharton School, University of Pennsylvania (USA), I have recently joined the research group EPIDERME at the University Paris XII (France) as a Junior Professor in Biostatistics.
         </div>
@@ -17,12 +17,10 @@ export default function AboutMe() {
             Research Interests
         </div>
         <div className="contentText">
-            My current works primarily concern the development of novel approaches for causally
-            interpretable meta-analysis and heterogeneity assessment. More recently, I have been 
-            working on the use of instrumental variables in difference-in-difference design, methods 
-            for mediation analysis and indirect treatment comparison in health technology assessment. 
-            One important feature of my research is the application of semi-parametric theory to allow 
-            the use of flexible, data-adaptive or machine learning algorithms to address causal problems. 
+            My current works primarily concern the development of novel approaches for data fusion, mediation analysis,  
+            meta-analysis and heterogeneity assessment. More recently, I have been working on methods to address unmeasured confounders, 
+            policy learning and indirect treatment comparisons. One important feature of my research is the application of 
+            semi-parametric theory to develop flexible, data-adaptive estimation strategies for causal parameters. 
             I am also interested in the applications of causal methods to clinical questions in medicine, 
             psychology and pharmacoepidemiology.
         </div>
@@ -32,34 +30,34 @@ export default function AboutMe() {
         <div className="contentText">
             <ul>
                 <li>
-                    New Faculty Travel Grant Award, Institute of Mathematical Statistics – Aug 2022
+                    New Faculty Travel Grant Award, Institute of Mathematical Statistics – Aug 2022.
                 </li>
                 <li>
-                    Junior Researcher Travel Grant, National Science Foundation – May 2022
+                    Junior Researcher Travel Grant, US National Science Foundation – May 2022.
                 </li>
                 <li>
-                    Third Prize, 2019 TEDxGhent Research Rally – Nov 2019
+                    Third Prize, TEDxGhent Research Rally – Nov 2019.
                 </li>
                 <li>
-                    Norman Breslow Award, Section on Statistics in Epidemiology, American Statistical Association – Aug 2019
+                    Norman Breslow Award, Section on Statistics in Epidemiology, American Statistical Association – Aug 2019.
                 </li>
                 <li>
-                    Student Conference Award, International Society of Clinical Biostatistics – Aug 2018
+                    Student Conference Award, International Society of Clinical Biostatistics – Aug 2018.
                 </li>
                 <li>
-                    Marie Sklodowska-Curie Actions funding (35.000 EUR/year), project MiRoR – 2016-2019
+                    Marie Sklodowska-Curie Actions funding (35.000 EUR/year), project MiRoR – 2016-2019.
                 </li>
                 <li>
-                    Ile-de-France International Master Scholarship (10.400 EUR/year) – 2014-2016
+                    Ile-de-France International Master Scholarship (10.400 EUR/year) – 2014-2016.
                 </li>
                 <li>
-                    Eiffel Excellence Scholarship from French Ministry of Europe and Foreign (11.000 E/year) – 2015-2016
+                    Eiffel Excellence Scholarship from French Ministry of Europe and Foreign (11.000 E/year) – 2015-2016.
                 </li>
                 <li>
-                    Agence Universitaire de la Francophonie Master Scholarship (7.000 EUR/year) – 2014-2015
+                    Agence Universitaire de la Francophonie Master Scholarship (7.000 EUR/year) – 2014-2015.
                 </li>
                 <li>
-                    Valedictorian. Class of 2014, Hanoi University of Pharmacy – June 2014
+                    Valedictorian. Class of 2014, Hanoi University of Pharmacy – June 2014.
                 </li>
             </ul>
         </div>
