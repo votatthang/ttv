867d05bf02fe72155df332bb5adfd31dd0ff84f2
votatthang <59831890+votatthang@users.noreply.github.com>
2024-01-18 22:11:43 +0100

    Update MyTeam.js

--- src/Pages/MyTeam/MyTeam.js
+++ src/Pages/MyTeam/MyTeam.js
@@ -9,16 +9,16 @@ export default function MyTeam() {
             <ul>
                 <li>
                     Khoi Le. PhD student in Epidemiology, University Paris XII. 2023-2026
-                    Topic: Efficacy and safety of biologic treatments in psoriasis: causal inference
+                    <br/> Topic: Efficacy and safety of biologic treatments in psoriasis: causal inference
                     from observational data. Coadvisor: Emilie Sbidian.
                 </li>
                 <li>
                     Thomas Bettuzzi (mentee). PhD student in Epidemiology, University Paris XII. 2023-2024
-                    Topic: Integrating aggregated data in random-effect meta-analysis of individual-level data.
+                    <br/> Topic: Integrating aggregated data in random-effect meta-analysis of individual-level data.
                 </li>
                 <li>
                     Laetitia Tan-Lhernould (mentee). Biotatistician. Henri Mondor Hospital. 2023-2024
-                    Topic: Novel methods for indirect treatment comparisons in health technology assessment.
+                    <br/> Topic: Novel methods for indirect treatment comparisons in health technology assessment.
                 </li>
             </ul>
         </div>
@@ -29,12 +29,12 @@ export default function MyTeam() {
             <ul>
                 <li>
                     Bang Truong (mentee). PhD student in Epidemiology, Auburn University. 2021-2022
-                    Topic: A systematic review of population adusted-indirect comparisons in
+                    <br/> Topic: A systematic review of population adusted-indirect comparisons in
                     health technology assessment.
                 </li>
                 <li>
                     Luong Vuong (mentee). MSc. student in Epidemiology, Antwerp University. 2021-2022
-                    Topic: Methods for developing minimum reporting sets of patient characteristics
+                    <br/> Topic: Methods for developing minimum reporting sets of patient characteristics
                     in epidemiological research.
                 </li>
             </ul>
