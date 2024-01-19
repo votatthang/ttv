da629cbbc63944489e8534b3eba27cb9eaded85b
votatthang <59831890+votatthang@users.noreply.github.com>
2024-01-19 09:29:05 +0100

    Update Research.js

--- src/Pages/Research/Research.js
+++ src/Pages/Research/Research.js
@@ -8,8 +8,8 @@ export default function Research() {
     <div className="contentText">
       The integration of data and knowledge from several data sources is often known as data fusion. Across different research fields, data fusion is increasingly needed
       to satisfy the requirement of combining information from multiple databases, so as to improve our ability to perform statistical inferences. Within this realm, 
-      my works primarily concern the development of new methods to address the challenge of heterogeneity and restricted access to individual-level data
-      when fusing and/or borrowing information across different populations, e.g. in evidence synthesis or health technology assessment. 
+      my works primarily concern the development of new methods to address the challenge of heterogeneity and of restricted access to individual-level data, 
+      when borrowing information across different populations in evidence synthesis or health technology assessment. 
       In this direction, I strongly collaborate with Pr. Stijn Vansteelandt, Dr. Raphael Porcher, Dr. Ivan Diaz and Dr. Kara Rudolph.
     </div>
     <div className="title">
@@ -18,11 +18,11 @@ export default function Research() {
     <div className="contentText">
       Mediation analyses are statistical techniques commonly used to study the mechanisms via which exposures exert causal effects on outcomes. 
       The presence of intermediate confounders, also called recanting witnesses, is a long standing challenge that prevents 
-      a fine-grained assessment of diffrent causal pathways linking the exposure and the outcome. 
-      In a recent work, we develop a novel method for mediation analysis in settings with intermediate confounding, 
-      with guarantees that the causal parameters measure the target individual-level mechanisms. 
-      The method is based on recently proposed ideas that view causality as the transfer of information, and thus replace recanting witnesses by draws 
-      from their conditional distribution. This is a joint work with Dr. Ivan Diaz and Dr. Kara Rudolph.
+      a fine-grained assessment of diffrent mediating pathways. 
+      In a recent work, we develop a novel method to address intermediate confounding, 
+      with guarantees that the proposed causal parameters measure the target individual-level mechanisms. 
+      This method is based on recently proposed ideas that view causality as the transfer of information, and thus replace recanting witnesses by draws 
+      from their conditional distribution, which we call "recanting twins". This work is a fruiful collaboration with Dr. Ivan Diaz and Dr. Kara Rudolph.
     </div>
     <div className="title">
       Risk of Bias in Mediation Analysis (ROBIMA)
