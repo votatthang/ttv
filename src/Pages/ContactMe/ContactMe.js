dbe6bcfabbd57b6db241251a2b871cf7599af3aa
votatthang <59831890+votatthang@users.noreply.github.com>
2024-01-21 14:33:17 +0100

    Update ContactMe.js

--- src/Pages/ContactMe/ContactMe.js
+++ src/Pages/ContactMe/ContactMe.js
@@ -11,34 +11,34 @@ const ContactMe = () => {
         <div className="contentText">
             <ul>
                 <li>
-                    <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>, 
+                    <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>, 
                     Wiliams NT, Liu L, Rudolph KE, Diaz I. Recanting twins: addressing intermediate
                     confounding in mediation analysis - under review.
                 </li>
                 <li>
-                    <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>, 
+                    <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>, 
                         Ye T, Ertefaie A, Roy S, Flory J, Hennessy S, Vansteelandt S, Small D. Marginal
                     Structural Models for Instrumented Di erence in Di erence design - under revision.
                 </li>
                 <li>
-                    Roy S, Ye T, Ertefaie A, <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>, 
+                    Roy S, Ye T, Ertefaie A, <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>, 
                         Flory J, Hennessy S, Small D. Group Sequential Testing under
                     Instrumented Di erence-in-Di erence approach. Stat Med. doi:10.1002/sim.9836.
                 </li>
                 <li>
-                    <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>, 
+                    <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>, 
                         Davies H, Hackett R, Vansteelandt S. Longitudinal mediation analysis of time-to-event
                     outcome in the presence of competing risk. Lifetime Data Anal. 2022;28:380-400. doi:10.1007/
                     s10985-022-09555-7.
                 </li>                                                                        
                 <li> 
-                    <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>, 
+                    <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>, 
                         Porcher R, Vansteelandt S. Assessing the impact of case-mix heterogeneity in individual
                     participant data meta-analysis: Novel use of I2 statistic and prediction interval. Research
                     Methods in Medicine & Health Sciences. 2021;2(1):12-30.doi:10.1177/2632084320957207.
                 </li> 
                <li>
-                    <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>, 
+                    <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>, 
                         Porcher R, Chaimani A, Vansteelandt S. A Novel Approach for Identifying and Addressing
                     Case-Mix Heterogeneity in Individual Participant Data Meta-Analysis. Res Synth Methods.
                     2019;10(4):582-596. doi:10.1002/jrsm.1382.
@@ -51,54 +51,54 @@ const ContactMe = () => {
         <div className="contentText">
             <ul>
                 <li>
-                Truong B, Tran LAT, Pham T, Nguyen TA, <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>. 
+                Truong B, Tran LAT, Pham T, Nguyen TA, <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>. 
                         Population adjustment for indirect treatment
                 comparison in health technology assessment: a methodological systematic review. Res Syn
                 Meth. 2023;14(5):660-670. doi:10.1002/jrsm.1653.
                 </li>
                 <li>
                 Nguyen V, Sharp M, Superchi C, Baron G, Glonti K, Blanco D, Olsen M, 
-                    <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>, 
+                    <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>, 
                     Olarte C, Neveol A, Hren D, Ravaud P, Boutron I. Environmental influences on biomedical doctoral students'
                 research practices when facing dilemmas: vignette-based randomized control trials. Sci Rep 13,
                 16371 (2023). doi:10.1038/s41598-023-42121-1.
                 </li>
                 <li>
-                <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>. 
+                <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>. 
                     A cautionary note on the use of G-computation in population adjustment. Res Syn Meth.
                 2023;1-4. doi:10.1002/jrsm.1621.
                 </li>
                 <li>
-                van Lancker K, <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>, Akacha M. 
+                van Lancker K, <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>, Akacha M. 
                         Estimands in heath technology assessment: a causal
                 inference perspective. Stat Med. 2022;41(28):5577-5585. doi:10.1002/sim.9539.
                 </li>
                 <li>
-                Vuong ML, Tu PHT, Duong KL, <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>. 
+                Vuong ML, Tu PHT, Duong KL, <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>. 
                     Development of minimum reporting sets of patient
                 characteristics in epidemiological research: a methodological systematic review. Research Methods
                 in Medicine & Health Sciences. 2023;1-14. doi:10.1177/26320843231191777.
                 </li>
                 <li>
-                <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>, Cashin A, Superchi C, 
+                <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>, Cashin A, Superchi C, 
                     Tu PHT, Nguyen TB, Boutron I, MacKinnon D, Vanderweele T,
                 Lee H, Vansteelandt S. Current quality assessment practice in systematic reviews of mediation
                 studies: an overview of systematic reviews. J Clin Epidemiol. 2022;143:37-148. doi:10.1016/
                 j.jclinepi.2021.12.013.
                 </li>
                 <li> 
-                <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>, 
+                <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>, 
                     Vansteelandt S. Challenges in systematic reviews and meta-analyses of mediation studies.
                 Am J Epidemiol. 2022;191(6), 1098-1106. doi:10.1093/aje/kwac028.
                 </li>
                 <li>
-                <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>, Superchi C, Boutron I, Vansteelandt S. 
+                <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>, Superchi C, Boutron I, Vansteelandt S. 
                         The conduct and reporting of mediation analysis
                 in recently published randomized controlled trials: results from a methodological systematic
                 review. J Clin Epidemiol. 2020;117:78-88. doi:10.1016/j.jclinepi.2019.10.001.
                 </li>
                 <li>
-                <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>, 
+                <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>, 
                     Vivot A, Porcher R. Impact of Biomarker-based Design Strategies on the Risk of False-
                 Positive Findings in Targeted Therapy Evaluation. Clin Cancer Res. 2018;24(24):6257-6264.
                 doi:10.1158/1078-0432.CCR-18-0328.
@@ -111,13 +111,13 @@ const ContactMe = () => {
         <div className="contentText">
             <ul>
                 <li>
-                Hackett R, <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>, 
+                Hackett R, <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>, 
                     Vansteelandt S, Davies-Kershaw H. The role of loneliness on hearing 
                 ability and dementia: a novel mediation approach. J Am Geriatr Soc. 2023;71(9):2834-2844.
                 doi:10.1111/jgs.183962844.
                 </li>
                 <li>
-                Murillo C, <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en"> Vo T</a>, 
+                Murillo C, <a href="https://scholar.google.com/citations?user=VMFhSVcAAAAJ&hl=en">Vo T</a>, 
                     Vansteelandt S, Harrison L, Cagnie B, Coppieters I, Chys M, Timmers I,
                 Meeus M. How and for whom do cognitive behavioral therapies for chronic pain work? A
                 systematic review and meta-analysis of speci c moderators and mediators of treatment outcome
