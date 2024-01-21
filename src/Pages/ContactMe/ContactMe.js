a376d5a8c1df3b09d5807c484d688625f10485bf
votatthang <59831890+votatthang@users.noreply.github.com>
2024-01-21 11:48:45 +0100

    Update ContactMe.js

--- src/Pages/ContactMe/ContactMe.js
+++ src/Pages/ContactMe/ContactMe.js
@@ -8,20 +8,37 @@ const ContactMe = () => {
     return (
         <div className="Publications">
          <div className="title">
-            2023
+            Statistical Methods
         </div>
         <div className="contentText">
             <ul>
                 <li>
-                    Khoi Le. PhD student in Epidemiology, University Paris XII. 2023-2026.
-                    <br/> Coadvisor: Pr. Emilie Sbidian.
+                    Vo T, Wiliams NT, Liu L, Rudolph KE, Diaz I. Recanting twins: addressing intermediate
+                    confounding in mediation analysis - under review.
                     <br/> 
                 </li>
                 <li>
-                    Thomas Bettuzzi (mentee). PhD student in Epidemiology, University Paris XII. 2023-2024.
+                    Vo T, Ye T, Ertefaie A, Roy S, Flory J, Hennessy S, Vansteelandt S, Small D. Marginal
+                    Structural Models for Instrumented Di erence in Di erence design - under revision.
                 </li>
                 <li>
-                    Laetitia Tan-Lhernould (mentee). Biotatistician, Henri Mondor Hospital. 2023-2024.
+                    Roy S, Ye T, Ertefaie A, Vo T, Flory J, Hennessy S, Small D. Group Sequential Testing under
+                    Instrumented Di erence-in-Di erence approach. Stat Med. doi:10.1002/sim.9836.
+                </li>
+                <li>
+                    Vo T, Davies H, Hackett R, Vansteelandt S. Longitudinal mediation analysis of time-to-event
+                    outcome in the presence of competing risk. Lifetime Data Anal. 2022;28:380{400. doi:10.1007/
+                    s10985-022-09555-7.
+                <li/>                                                                        
+                <li> 
+                    Vo T, Porcher R, Vansteelandt S. Assessing the impact of case-mix heterogeneity in individual
+                    participant data meta-analysis: Novel use of I2 statistic and prediction interval. Research
+                    Methods in Medicine & Health Sciences. 2021;2(1):12-30.doi:10.1177/2632084320957207.
+               <li/> 
+               <li>
+                    Vo T, Porcher R, Chaimani A, Vansteelandt S. A Novel Approach for Identifying and Addressing
+                    Case-Mix Heterogeneity in Individual Participant Data Meta-Analysis. Res Synth Methods.
+                    2019;10(4):582-596. doi:10.1002/jrsm.1382.
                 </li>
             </ul>
         </div> 
