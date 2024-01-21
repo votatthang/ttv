a2149336cfbc11db5b54b7ed3bfb720c626ef921
votatthang <59831890+votatthang@users.noreply.github.com>
2024-01-21 11:35:21 +0100

    Update ContactMe.js

--- src/Pages/ContactMe/ContactMe.js
+++ src/Pages/ContactMe/ContactMe.js
@@ -6,8 +6,24 @@ const ContactMe = () => {
     
 
     return (
-        <div className="ContactMe">
-        Publications 
+         <div className="title">
+            2023
+        </div>
+        <div className="contentText">
+            <ul>
+                <li>
+                    Khoi Le. PhD student in Epidemiology, University Paris XII. 2023-2026.
+                    <br/> Coadvisor: Pr. Emilie Sbidian.
+                    <br/> 
+                </li>
+                <li>
+                    Thomas Bettuzzi (mentee). PhD student in Epidemiology, University Paris XII. 2023-2024.
+                </li>
+                <li>
+                    Laetitia Tan-Lhernould (mentee). Biotatistician, Henri Mondor Hospital. 2023-2024.
+                </li>
+            </ul>
+        </div> 
         </div>
 
     );
