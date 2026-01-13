0a8273b408a1ab56ec3a675067730d9a41524098
votatthang <59831890+votatthang@users.noreply.github.com>
2026-01-13 16:11:12 +0100

    Update Research.js

--- src/Pages/Research/Research.js
+++ src/Pages/Research/Research.js
@@ -3,41 +3,46 @@ import React from "react";
 export default function Research() {
   return (
     <div className="Research">
-    <div className="title">
-      Causal data fusion and its applications
+      <div className="title">
+        Causal data fusion and its applications
+      </div>
+
+      <div className="contentText">
+        The integration of data and knowledge from several data sources is often known as data fusion.
+        Across different research fields, data fusion is increasingly needed to combine information
+        from multiple databases, so as to improve our ability to perform statistical inferences.
+        Within this realm, my works primarily concern the development of new methods to address the
+        challenge of heterogeneity and of restricted access to individual-level data, when borrowing
+        information across different populations in evidence synthesis or health technology assessment.
+      </div>
+
+      <div className="title">
+        Risk of Bias in Mediation Analysis (ROBIMA)
+      </div>
+
+      <div className="contentText">
+        ROBIMA is a new initiative that involves international experts in psychology, epidemiology and
+        biostatistics, with aim to construct a consensus-based, high-quality tool for evaluating
+        methodological biases in mediation analyses. This project involves many world-leading mediation
+        experts such as Pr.{" "}
+        <a href="https://www.ugent.be/dass/en/research/vansteelandt">Stijn Vansteelandt</a>, Pr.{" "}
+        <a href="https://www.hsph.harvard.edu/tyler-vanderweele/">Tyler Vanderweele</a>, Pr.{" "}
+        <a href="https://search.asu.edu/profile/28257">David MacKinnon</a>, Dr.{" "}
+        <a href="https://www.ndorms.ox.ac.uk/team/hopin-lee">Hopin Lee</a>, Dr.{" "}
+        <a href="https://neura.edu.au/researchers/aidan-cashin">Aidan Cashin</a>, among many others.
+      </div>
+
+      <div className="title">
+        Methods to address (un)measured confounding
+      </div>
+
+      <div className="contentText">
+        Finally, I am particularly interested in developing new methods, as well as improving current
+        methods to address measured and unmeasured confounders. For instance, we are currently working
+        on projects to combine matching and countermatching in nested case-control design, to extend
+        the difference-in-differences (DID) design to a survival outcome setting, or to develop
+        alternatives when the parallel trends assumption in DID is violated.
+      </div>
     </div>
-    <div className="contentText">
-      The integration of data and knowledge from several data sources is often known as data fusion. Across different research fields, data fusion is increasingly needed
-      to combine information from multiple databases, so as to improve our ability to perform statistical inferences. Within this realm, 
-      my works primarily concern the development of new methods to address the challenge of heterogeneity and of restricted access to individual-level data, 
-      when borrowing information across different populations in evidence synthesis or health technology assessment. 
-    </div>
-        
-    <div className="title">
-      Risk of Bias in Mediation Analysis (ROBIMA)
-    </div>
-    <div className="contentText">
-      ROBIMA is a new initiative that involves international experts in psychology, epidemiology and biostatistics, with aim to construct a consensus-based, 
-      high-quality tool for evaluating methodological biases in mediation analyses. This project involves many world-leading mediation experts such as 
-      Pr. <a href="https://www.ugent.be/dass/en/research/vansteelandt">Stijn Vansteelandt</a>, 
-      Pr. <a href="https://www.hsph.harvard.edu/tyler-vanderweele/">Tyler Vanderweele</a>, 
-      Pr. <a href="https://search.asu.edu/profile/28257">David MacKinnon</a>, 
-      Dr. <a href="https://www.ndorms.ox.ac.uk/team/hopin-lee">Hopin Lee</a>, 
-      Dr. <a href="https://neura.edu.au/researchers/aidan-cashin">Aidan Cashin</a>, among many others.
-    </div>
-        
-    <div className="title">
-      Methods to address (un)measured confounding
-    </div>
-    <div className="contentText">
-      Finally, I am particularly interested in developing new methods, as well as improving current methods to address measured and unmeasured confounders.
-      For instance, we are currently working on projects to combine matching and countermatching in nested case-control design, 
-      to extend the difference-in-differences (DID) design to a survival outcome setting,
-        or to develop alternatives when the parallel trends assumption in DID is violated.
-  </div>
-</div>
   );
 }
-
-
-
